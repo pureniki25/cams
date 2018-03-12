2936340d1404c7afccf7d7e572564aa15ad5d618
dengzhiming <2081122110>
2018-03-12 14:15:42 +0800

    删除了evaluation_amount

--- alms-base/src/main/java/com/hongte/alms/base/entity/CarBasic.java
+++ alms-base/src/main/java/com/hongte/alms/base/entity/CarBasic.java
@@ -43,12 +43,7 @@ public class CarBasic extends Model<CarBasic> {
 	@TableField("license_plate_number")
 	@ApiModelProperty(required= true,value = "车牌号 ")
 	private String licensePlateNumber;
-    /**
-     * 评估金额 
-     */
-	@TableField("evaluation_amount")
-	@ApiModelProperty(required= true,value = "评估金额 ")
-	private BigDecimal evaluationAmount;
+
     /**
      * 车身颜色 
      */
@@ -267,13 +262,6 @@ public class CarBasic extends Model<CarBasic> {
 		this.licensePlateNumber = licensePlateNumber;
 	}
 
-	public BigDecimal getEvaluationAmount() {
-		return evaluationAmount;
-	}
-
-	public void setEvaluationAmount(BigDecimal evaluationAmount) {
-		this.evaluationAmount = evaluationAmount;
-	}
 
 	public String getColor() {
 		return color;
@@ -550,7 +538,6 @@ public class CarBasic extends Model<CarBasic> {
 			" businessId=" + businessId +
 			", ownerName=" + ownerName +
 			", licensePlateNumber=" + licensePlateNumber +
-			", evaluationAmount=" + evaluationAmount +
 			", color=" + color +
 			", brandName=" + brandName +
 			", model=" + model +
