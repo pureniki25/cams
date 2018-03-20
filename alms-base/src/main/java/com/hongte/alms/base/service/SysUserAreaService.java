c4ff9bd413bd10b29f5ea42d7cf4bf0b1114f7df
wangjiguang <wangjiguang@hongte.info>
2018-03-20 17:55:07 +0800

    用户区域设置

--- alms-base/src/main/java/com/hongte/alms/base/service/SysUserAreaService.java
+++ alms-base/src/main/java/com/hongte/alms/base/service/SysUserAreaService.java
@@ -1,7 +1,11 @@
 package com.hongte.alms.base.service;
 
 import com.hongte.alms.base.entity.SysUserArea;
+import com.hongte.alms.base.vo.module.UserAreaReq;
+import com.hongte.alms.base.vo.module.UserAreaVO;
 import com.hongte.alms.common.service.BaseService;
+import com.hongte.alms.common.vo.PageRequest;
+import com.hongte.alms.common.vo.PageResult;
 
 import java.util.List;
 
@@ -21,4 +25,13 @@ public interface SysUserAreaService extends BaseService<SysUserArea> {
      * @return
      */
     List<String> selectUserAreas(String userId);
+    
+    /**
+     * 分页获取用户管理的区域
+     * @author 王继光
+     * 2018年3月20日 上午11:16:13
+     * @param req
+     * @return
+     */
+    PageResult<UserAreaVO> page(UserAreaReq req);
 }
