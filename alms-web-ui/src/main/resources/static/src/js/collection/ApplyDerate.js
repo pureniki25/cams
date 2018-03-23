e505c0b6459f70ec4c1d39f3c2828dbdca9b6fa7
huweiqian <huweiqian@hongte.info>
2018-03-23 20:48:09 +0800

    修复BUG

--- alms-web-ui/src/main/resources/static/src/js/collection/ApplyDerate.js
+++ alms-web-ui/src/main/resources/static/src/js/collection/ApplyDerate.js
@@ -30,6 +30,18 @@ var setFormValidate = {
     ],
     realReceiveMoney: [
         {pattern:/^[0-9]+(.[0-9]{1,2})?$/,   message: '请填写不超过两位小数的数字', trigger: 'blur'}
+    ],
+    derateReson: [
+    	{required: true, message: '必填', trigger: 'blur'}  ,
+    	{
+        	validator: function (rule, value, callback, source, options) {
+	        	if (value.length > 1000 ) {
+	        		callback(new Error('字数长度不能超过500'));
+	        	} else {
+	        	callback();//校验通过
+	        	}
+        	}
+        }
     ]
 };
 
