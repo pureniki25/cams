d92b40e3c2c582c4a48efca1ed62c5eb0c3decd1
chenzesheng <chenzesheng@hongte.info>
2018-03-26 10:46:49 +0800

    增加银行代扣枚举

--- alms-base/src/main/java/com/hongte/alms/base/enums/PlatformEnum.java
+++ alms-base/src/main/java/com/hongte/alms/base/enums/PlatformEnum.java
@@ -15,7 +15,9 @@ public enum PlatformEnum implements IEnum {
     YS_FORM(1,"银盛绑卡"),
     FY_FORM(2,"富友绑卡"),
     BF_FORM(3,"宝付代扣"),
-    AN_FORM(4,"爱农代扣");
+    AN_FORM(4,"爱农代扣"),
+    YH_FORM(5,"银行代扣");
+
 
     private int value;
     private String name;
