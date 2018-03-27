33ea922bae7129a40c65d9c4a7dc31813b03b300
zengkun <zengkun5200@163.com>
2018-03-27 15:47:55 +0800

    信贷 结清  结清撤销  调用接口

---
+++ alms-base/src/main/java/com/hongte/alms/base/mapper/CarBasicStatusLogMapper.java
@@ -0,0 +1,16 @@
+package com.hongte.alms.base.mapper;
+
+import com.hongte.alms.base.entity.CarBasicStatusLog;
+import com.hongte.alms.common.mapper.SuperMapper;
+
+/**
+ * <p>
+ * 车辆基本信息状态变更记录表 Mapper 接口
+ * </p>
+ *
+ * @author 曾坤
+ * @since 2018-03-26
+ */
+public interface CarBasicStatusLogMapper extends SuperMapper<CarBasicStatusLog> {
+
+}
--- alms-base/src/main/java/com/hongte/alms/base/service/CarBasicService.java
+++ alms-base/src/main/java/com/hongte/alms/base/service/CarBasicService.java
@@ -13,4 +13,10 @@ import com.hongte.alms.common.service.BaseService;
  */
 public interface CarBasicService extends BaseService<CarBasic> {
 
+    //更新车辆状态为已结清
+    public boolean updateCarStatusToSettled(String businessId);
+
+    //将车辆状态还原为已结清之前的状态
+    public boolean revokeCarStatus(String  businessId);
+
 }
