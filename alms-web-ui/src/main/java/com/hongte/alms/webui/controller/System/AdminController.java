be4eb71a159fd86602be0046b76d1014e061bdb4
huweiqian <huweiqian@hongte.info>
2018-04-09 09:20:07 +0800

    调试BUG

--- alms-web-ui/src/main/java/com/hongte/alms/webui/controller/System/AdminController.java
+++ alms-web-ui/src/main/java/com/hongte/alms/webui/controller/System/AdminController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminController {
 	@RequestMapping("/admin")
 	public String syncCollection() {
-		return "/system/admin";
+		return "/System/admin";
 	}
 }
