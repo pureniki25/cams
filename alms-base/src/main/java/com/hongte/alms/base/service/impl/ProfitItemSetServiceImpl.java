1237f99aac258d638fe27c329f27dbcd57943b53
chenzesheng <chenzesheng@hongte.info>
2018-05-03 16:06:38 +0800

    分润设置

--- alms-base/src/main/java/com/hongte/alms/base/service/impl/ProfitItemSetServiceImpl.java
+++ alms-base/src/main/java/com/hongte/alms/base/service/impl/ProfitItemSetServiceImpl.java
@@ -1,20 +1,26 @@
 package com.hongte.alms.base.service.impl;
 
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.hongte.alms.base.entity.BasicBusinessType;
 import com.hongte.alms.base.entity.ProfitFeeSet;
 import com.hongte.alms.base.entity.ProfitItemSet;
 import com.hongte.alms.base.entity.RepaymentBizPlanListDetail;
 import com.hongte.alms.base.entity.SysParameter;
 import com.hongte.alms.base.mapper.ProfitItemSetMapper;
+import com.hongte.alms.base.service.ProfitFeeSetService;
 import com.hongte.alms.base.service.ProfitItemSetService;
 import com.hongte.alms.common.service.impl.BaseServiceImpl;
 import com.hongte.alms.common.util.StringUtil;
 import com.ht.ussp.bean.LoginUserInfoHelper;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -32,6 +38,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class ProfitItemSetServiceImpl extends BaseServiceImpl<ProfitItemSetMapper, ProfitItemSet> implements ProfitItemSetService {
     @Autowired
     LoginUserInfoHelper loginUserInfoHelper;
+    
+    @Autowired
+	@Qualifier("ProfitFeeSetService")
+	ProfitFeeSetService profitFeeSetService;
 	@Override
 	public void saveItemTypes(List<SysParameter> itemTypes, Integer businessTypeId) {
 		ProfitItemSet itemSet=new ProfitItemSet();
@@ -52,6 +62,23 @@ public class ProfitItemSetServiceImpl extends BaseServiceImpl<ProfitItemSetMappe
 
 		updateBatchById(itemTypes);
 		
+	}
+	@Override
+	public Map<String, Object> getLevel(String businessTypeId, Integer itemType, String feeId) {
+		Map <String,Object> map=new HashMap<String, Object>();
+		ProfitItemSet itemSet=selectOne(new EntityWrapper<ProfitItemSet>().eq("item_type", itemType).eq("business_type_id", businessTypeId));
+		
+		if(itemSet!=null) {
+			map.put("itemLevel", itemSet.getItemLevel());
+			ProfitFeeSet feeSet=profitFeeSetService.selectOne(new EntityWrapper<ProfitFeeSet>().eq("profit_item_set_id", itemSet.getProfitItemSetId()).eq("fee_id", feeId));
+			if(feeSet!=null) {
+				map.put("feeLevel", feeSet.getFeeLevel());
+			}else{
+				map.put("feeLevel", itemSet.getItemLevel());
+			}
+		}
+		
+		return map;
 	}
 
 
