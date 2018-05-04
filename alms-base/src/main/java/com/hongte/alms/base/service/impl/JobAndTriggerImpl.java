479a23146aea4dd18b0e09a2720fcc36c699c64d
chenzesheng <chenzesheng@hongte.info>
2018-05-04 09:58:20 +0800

    定时器计算逾期天数

--- alms-base/src/main/java/com/hongte/alms/base/service/impl/JobAndTriggerImpl.java
+++ alms-base/src/main/java/com/hongte/alms/base/service/impl/JobAndTriggerImpl.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 
-@Service()
+@Service
 public class JobAndTriggerImpl extends BaseServiceImpl<JobAndTriggerMapper, JobAndTrigger> implements IJobAndTriggerService {
 
 	@Autowired
