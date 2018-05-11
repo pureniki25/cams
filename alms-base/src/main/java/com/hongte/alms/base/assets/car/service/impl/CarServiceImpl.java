9593184c1d97bec247ffcc4a79587d2c490ff775
zengkun <zengkun5200@163.com>
2018-05-11 09:23:28 +0800

    修改打印级别为info

--- alms-base/src/main/java/com/hongte/alms/base/assets/car/service/impl/CarServiceImpl.java
+++ alms-base/src/main/java/com/hongte/alms/base/assets/car/service/impl/CarServiceImpl.java
@@ -579,7 +579,7 @@ public class CarServiceImpl  implements CarService {
 			try {
 				if(MsgTemplateEnableEnum.ENABLE.getValue().equals(msgTemplate.getEnableFlag())) {//发送开关
 					Result eipResult=eipRemote.sendSms(vo);
-					logger.debug("EIP返回信息,code="+eipResult.getReturnCode()+",msg="+eipResult.getMsg());
+					logger.info("EIP返回信息,code="+eipResult.getReturnCode()+",msg="+eipResult.getMsg());
 					if(eipResult==null||!"0000".equals(eipResult.getReturnCode())) {
 						logger.error("调用EIP系统发送短信失败,eipResult="+eipResult==null?null:"调用EIP系统发送短信返回码："+eipResult.getReturnCode());
 						throw new AlmsBaseExcepiton( "调用EIP系统发送短信失败"); 
