a06e23cb5796d2fcb5d8e245cdf4874a735dccd2
wangjiguang <wangjiguang@hongte.info>
2018-05-24 10:02:13 +0800

    财务管理-确认还款-添加结余项实体类/DAO

---
+++ alms-base/src/main/java/com/hongte/alms/base/service/impl/AccountantOverRepayLogServiceImpl.java
@@ -0,0 +1,20 @@
+package com.hongte.alms.base.service.impl;
+
+import com.hongte.alms.base.entity.AccountantOverRepayLog;
+import com.hongte.alms.base.mapper.AccountantOverRepayLogMapper;
+import com.hongte.alms.base.service.AccountantOverRepayLogService;
+import com.hongte.alms.common.service.impl.BaseServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 业务结余暂收日志表 服务实现类
+ * </p>
+ *
+ * @author 王继光
+ * @since 2018-05-24
+ */
+@Service
+public class AccountantOverRepayLogServiceImpl extends BaseServiceImpl<AccountantOverRepayLogMapper, AccountantOverRepayLog> implements AccountantOverRepayLogService {
+
+}
