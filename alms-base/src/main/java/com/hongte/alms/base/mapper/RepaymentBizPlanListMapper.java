285732eda6a04b6a9d5a9ed6b6c317fba3792553
wangjiguang <wangjiguang@hongte.info>
2018-05-28 18:36:07 +0800

    财务管理-代扣确认

--- alms-base/src/main/java/com/hongte/alms/base/mapper/RepaymentBizPlanListMapper.java
+++ alms-base/src/main/java/com/hongte/alms/base/mapper/RepaymentBizPlanListMapper.java
@@ -3,6 +3,7 @@ package com.hongte.alms.base.mapper;
 import com.hongte.alms.base.dto.FinanceManagerListReq;
 import com.hongte.alms.base.dto.RepaymentPlanInfoDTO;
 import com.hongte.alms.base.entity.RepaymentBizPlanList;
+import com.hongte.alms.base.vo.finance.ConfirmWithholdListVO;
 import com.hongte.alms.base.vo.module.FinanceManagerListVO;
 import com.hongte.alms.common.mapper.SuperMapper;
 import org.apache.ibatis.annotations.Param;
@@ -99,4 +100,13 @@ public interface RepaymentBizPlanListMapper extends SuperMapper<RepaymentBizPlan
      * @return
      */
     BigDecimal caluBizPlanListUnpaid(String planListId);
+    
+    /**
+     * 查询业务的代扣确认列表信息
+     * @author 王继光
+     * 2018年5月28日 下午4:09:31
+     * @param businessId
+     * @return
+     */
+    List<ConfirmWithholdListVO> listConfirmWithhold(String businessId);
 }
