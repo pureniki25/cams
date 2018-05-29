6d87231cd26bd975a73e7660db1630c94df10a81
chenzesheng <chenzesheng@hongte.info>
2018-05-29 14:49:35 +0800

    表名修改

--- alms-base/src/main/java/com/hongte/alms/base/entity/ProfitFeeSet.java
+++ alms-base/src/main/java/com/hongte/alms/base/entity/ProfitFeeSet.java
@@ -19,7 +19,7 @@ import io.swagger.annotations.ApiModelProperty;
  * @since 2018-05-02
  */
 @ApiModel
-@TableName("tb_profit_fee_set")
+@TableName("tb_sys_profit_fee_set")
 public class ProfitFeeSet extends Model<ProfitFeeSet> {
 
     private static final long serialVersionUID = 1L;
