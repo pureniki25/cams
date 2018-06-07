17172e9c895c8c545dfc8ec7ac137fc85a6f4e26
chenzesheng <chenzesheng@hongte.info>
2018-06-07 20:17:00 +0800

    滞纳金日志

--- alms-base/src/main/java/com/hongte/alms/base/service/impl/RepaymentProjPlanListServiceImpl.java
+++ alms-base/src/main/java/com/hongte/alms/base/service/impl/RepaymentProjPlanListServiceImpl.java
@@ -501,6 +501,7 @@ public class RepaymentProjPlanListServiceImpl extends
 		 * @return
 		 */
 		private Date getPlatformDuedate(String projId,String periods) {
+			logger.debug("====================标ID:"+projId+"获取平台标的期数为："+periods+"的还款日日期开始=======================================");
 		 	Map<String, Object> paramMap = new HashMap<>();
 			paramMap.put("projectId",projId);
 			paramMap.put("type", "1");
