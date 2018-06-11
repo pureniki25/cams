2975cda5a986dee534a879c707f763d61798f40a
zengkun <zengkun5200@163.com>
2018-06-11 09:26:10 +0800

    1.贷后管理页面，设置电催/催收后，刷新列表 2.设置财务跟单人员

--- alms-web-ui/src/main/resources/static/src/js/comm.js
+++ alms-web-ui/src/main/resources/static/src/js/comm.js
@@ -5,7 +5,6 @@ var closePareantLayer = function(){
     if (typeof (parent.layer) != 'undefined') {
         var index = parent.layer.getFrameIndex(window.name); //获取窗口索引
         parent.layer.close(index);
-        parent.layer.success();
     }
 
 }
