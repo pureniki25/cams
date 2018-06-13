8518834bd09682dccbdf56a2031946a446081307
zengkun <zengkun5200@163.com>
2018-06-13 13:56:56 +0800

    对接合规化还款，接口

--- alms-base/src/main/java/com/hongte/alms/base/service/impl/JobAndTriggerImpl.java
+++ alms-base/src/main/java/com/hongte/alms/base/service/impl/JobAndTriggerImpl.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 
-@Service
+@Service("IJobAndTriggerService")
 public class JobAndTriggerImpl extends BaseServiceImpl<JobAndTriggerMapper, JobAndTrigger> implements IJobAndTriggerService {
 
 	@Autowired
