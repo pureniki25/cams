c4354e6b3755d468a6b696b81a5bc19b214e30ca
chenzesheng <chenzesheng@hongte.info>
2018-06-13 20:04:52 +0800

    减免问题

--- alms-base/src/main/java/com/hongte/alms/base/service/impl/ApplyDerateProcessServiceImpl.java
+++ alms-base/src/main/java/com/hongte/alms/base/service/impl/ApplyDerateProcessServiceImpl.java
@@ -381,10 +381,13 @@ public class ApplyDerateProcessServiceImpl extends BaseServiceImpl<ApplyDeratePr
     // List<ApplyDerateType>  applyDerateTypeList=applyDerateTypeService.selectList(new EntityWrapper<ApplyDerateType>().eq("apply_derate_process_id",derateInfo.getApplyDerateProcessId()));
      List<ApplyDerateType>  applyDerateTypeList=applyDerateTypeService.getApplyTypeByBusinessIdAndCrpId(derateInfo.getBusinessId(), derateInfo.getCrpId());//这个方法查出如果同一期减免多次费用的总额
        for(ApplyDerateType applyDerateType: applyDerateTypeList) {
-    	   derateFee=new DerateFee();
-    	   derateFee.setAmount(applyDerateType.getDerateMoney());
-    	   derateFee.setFeeId(applyDerateType.getFeeId());
-    	   derateFeeList.add(derateFee);
+    	   if(applyDerateType!=null) {
+    		   derateFee=new DerateFee();
+        	   derateFee.setAmount(applyDerateType.getDerateMoney());
+        	   derateFee.setFeeId(applyDerateType.getFeeId());
+        	   derateFeeList.add(derateFee);  
+    	   }
+    	 
     	
        }
        reqParam.setDerateFeeList(derateFeeList);
