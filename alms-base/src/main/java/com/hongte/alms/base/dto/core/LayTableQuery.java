a22881884dc89a32720ceaeb59612d85fee97981
zghlx <zghlx@qq.com>
2018-06-17 10:31:30 +0800

    专用于LayuiTable的后端分页类

---
+++ alms-base/src/main/java/com/hongte/alms/base/dto/core/LayTableQuery.java
@@ -0,0 +1,57 @@
+package com.hongte.alms.base.dto.core;
+
+import java.util.Map;
+
+/**
+ * 专用于LayuiTable的后端分页类
+ * <p>
+ * Created by 张贵宏 on 2018/6/17 10:25
+ */
+public class LayTableQuery {
+
+    private Map<String, String> search;
+    private Integer limit;
+    private Integer page;
+    private String orderField = "create_time";
+    private String orderDirection = "desc";
+
+    public Map<String, String> getSearch() {
+        return search;
+    }
+
+    public void setSearch(Map<String, String> search) {
+        this.search = search;
+    }
+
+    public Integer getLimit() {
+        return limit;
+    }
+
+    public void setLimit(Integer limit) {
+        this.limit = limit;
+    }
+
+    public Integer getPage() {
+        return page - 1;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public String getOrderField() {
+        return orderField;
+    }
+
+    public void setOrderField(String orderField) {
+        this.orderField = orderField;
+    }
+
+    public String getOrderDirection() {
+        return orderDirection;
+    }
+
+    public void setOrderDirection(String orderDirection) {
+        this.orderDirection = orderDirection;
+    }
+}
