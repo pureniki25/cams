cf286623fd797644508c4c8679a23c56e9b9aa91
张贵宏 <zghlx@qq.com>
2018-06-17 17:15:33 +0800

    财务跟单分页查询

--- alms-base/src/main/java/com/hongte/alms/base/service/impl/SysFinancialOrderServiceImpl.java
+++ alms-base/src/main/java/com/hongte/alms/base/service/impl/SysFinancialOrderServiceImpl.java
@@ -1,10 +1,14 @@
 package com.hongte.alms.base.service.impl;
 
+import com.baomidou.mybatisplus.plugins.Page;
 import com.hongte.alms.base.entity.SysFinancialOrder;
 import com.hongte.alms.base.mapper.SysFinancialOrderMapper;
 import com.hongte.alms.base.service.SysFinancialOrderService;
+import com.hongte.alms.base.vo.finance.SysFinancialOrderVO;
 import com.hongte.alms.common.service.impl.BaseServiceImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +21,17 @@ import org.springframework.stereotype.Service;
 @Service("SysFinancialOrderService")
 public class SysFinancialOrderServiceImpl extends BaseServiceImpl<SysFinancialOrderMapper, SysFinancialOrder> implements SysFinancialOrderService {
 
+    /**
+     *  自定义分页查询
+     *
+     * @param [page, businessTypeId, areaId, companyId, userName]
+     * @return com.baomidou.mybatisplus.plugins.Page<com.hongte.alms.base.vo.finance.SysFinancialOrderVO>
+     * @author 张贵宏
+     * @date 2018/6/17 15:25
+     */
+    @Override
+    public Page<SysFinancialOrderVO> search(Page<SysFinancialOrderVO> page, Integer businessTypeId, String areaId, String companyId, String userName) {
+        List<SysFinancialOrderVO>  voList = ((SysFinancialOrderMapper)super.baseMapper).search(page, businessTypeId, areaId, companyId, userName);
+        return page.setRecords(voList);
+    }
 }
