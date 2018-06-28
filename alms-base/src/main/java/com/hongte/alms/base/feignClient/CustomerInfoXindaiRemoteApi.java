e7171b111b4402a470af4ef78f9bd7621ccbeaac
chenzesheng <chenzesheng@hongte.info>
2018-06-28 14:02:00 +0800

    执行代扣获取银行卡信息修改

--- alms-base/src/main/java/com/hongte/alms/base/feignClient/CustomerInfoXindaiRemoteApi.java
+++ alms-base/src/main/java/com/hongte/alms/base/feignClient/CustomerInfoXindaiRemoteApi.java
@@ -1,14 +1,9 @@
 package com.hongte.alms.base.feignClient;
 
 
-import com.hongte.alms.base.feignClient.dto.BankCardInfo;
-import com.hongte.alms.base.feignClient.dto.CustomerInfoDto;
 import com.hongte.alms.common.result.Result;
 
-import java.util.List;
-
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,7 +21,7 @@ public interface CustomerInfoXindaiRemoteApi {
      * 根据身份证号码获取客户银行卡信息
      */
     @RequestMapping(value = "/CustomerBankcardController/getBankcardInfo",headers = {"app=ALMS", "content-type=application/json"},method = RequestMethod.GET)
-    List<BankCardInfo> getBankcardInfo(@RequestParam("identityCard") String identityCard) throws Exception;
+    Result getBankcardInfo(@RequestParam("identityCard") String identityCard) throws Exception;
 
 
     }
