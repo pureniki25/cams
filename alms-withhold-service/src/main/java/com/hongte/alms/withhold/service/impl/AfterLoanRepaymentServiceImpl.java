d8d6b179d025f21069969b71c65ff60df3161bfa
林子尧 <linziyao@hongte.info>
2018-06-29 16:27:07 +0800

    调整代扣结果返回信息的格式

--- alms-withhold-service/src/main/java/com/hongte/alms/withhold/service/impl/AfterLoanRepaymentServiceImpl.java
+++ alms-withhold-service/src/main/java/com/hongte/alms/withhold/service/impl/AfterLoanRepaymentServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hongte.alms.withhold.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.hongte.alms.base.entity.BasicBusiness;
 import com.hongte.alms.base.entity.RepaymentBizPlanList;
@@ -56,7 +57,13 @@ public class AfterLoanRepaymentServiceImpl implements AfterLoanRepaymentService 
     	if(repaymentBizPlanList!=null){
     		//判断是否贷后代扣
             if(repaymentBizPlanList.getSrcType()==null || repaymentBizPlanList.getSrcType().intValue()==1){
-                return withHoldingClient.repayAssignBank(repaymentBizPlanList.getOrigBusinessId(),afterId,bankCard);
+                Result result = withHoldingClient.repayAssignBank(repaymentBizPlanList.getOrigBusinessId(),afterId,bankCard);
+                Object object = JSON.parse(result.getData().toString());
+                Result resultObj=new Result();
+                resultObj.setCode(result.getCode());
+                resultObj.setMsg(result.getMsg());
+                resultObj.setData(object);
+                return resultObj;
             }else {
                  withholdingService.appWithholding(repaymentBizPlanList);
                  Result result=new Result();
