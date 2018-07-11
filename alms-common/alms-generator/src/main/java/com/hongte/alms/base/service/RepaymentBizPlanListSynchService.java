ab0b3b1526b157c2e8e8a2ca6d673934f1d9dd0b
liuzhengquan@hongte.info <liu@2018>
2018-07-11 10:22:23 +0800

    财务管理优化

---
+++ alms-common/alms-generator/src/main/java/com/hongte/alms/base/service/RepaymentBizPlanListSynchService.java
@@ -0,0 +1,16 @@
+package com.hongte.alms.base.service;
+
+import com.hongte.alms.base.entity.RepaymentBizPlanListSynch;
+import com.hongte.alms.common.service.BaseService;
+
+/**
+ * <p>
+ * 业务还款计划列表 服务类
+ * </p>
+ *
+ * @author 刘正全
+ * @since 2018-07-09
+ */
+public interface RepaymentBizPlanListSynchService extends BaseService<RepaymentBizPlanListSynch> {
+
+}
