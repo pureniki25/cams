6304dd2117f8de6c73357cce132ba964cd4d91fb
chenzesheng <chenzesheng@hongte.info>
2018-07-12 15:53:36 +0800

    快钱

--- alms-base/src/main/java/com/hongte/alms/base/feignClient/EipRemote.java
+++ alms-base/src/main/java/com/hongte/alms/base/feignClient/EipRemote.java
@@ -14,6 +14,7 @@ import com.hongte.alms.base.dto.compliance.TdDepaymentEarlierDTO;
 import com.hongte.alms.base.feignClient.dto.AddProjectTrackReqDto;
 import com.hongte.alms.base.feignClient.dto.BankRechargeReqDto;
 import com.hongte.alms.base.feignClient.dto.BaofuRechargeReqDto;
+import com.hongte.alms.base.feignClient.dto.KuaiQianRechargeReqDto;
 import com.hongte.alms.base.feignClient.dto.YiBaoRechargeReqDto;
 import com.hongte.alms.base.vo.comm.SmsVo;
 import com.ht.ussp.core.Result;
@@ -234,6 +235,44 @@ public interface EipRemote {
 	@RequestMapping(value = "/eip/td/repayment/queryRepaymentEarlier", headers = { "app=ALMS",
 	"content-type=application/json" }, method = RequestMethod.POST)
 	Result queryRepaymentEarlier(@RequestBody Map<String, Object> paramMap);
+	
+	
+	
+	
+	/**
+	 * 快钱签约申请
+	 * @param paramMap
+	 * @return
+	 */
+	@RequestMapping(value = "/eip/kq/signApply", headers = { "app=ALMS",
+	"content-type=application/json" }, method = RequestMethod.POST)
+	Result signApply(@RequestBody Map<String, Object> paramMap);
+	
+	
+	
+	
+	
+	/**
+	 * 快钱签约申请确认
+	 * @param paramMap
+	 * @return
+	 */
+	@RequestMapping(value = "/eip/kq/signApplyVerify", headers = { "app=ALMS",
+	"content-type=application/json" }, method = RequestMethod.POST)
+	Result signApplyVerify(@RequestBody Map<String, Object> paramMap);
+	
+	
+	
+	
+	/**
+	 * 快钱支付
+	 * @param paramMap
+	 * @return
+	 */
+	@RequestMapping(value = "/eip/kq/pay", headers = { "app=ALMS",
+	"content-type=application/json" }, method = RequestMethod.POST)
+	Result pay(@RequestBody  KuaiQianRechargeReqDto kuaiQianRechargeReqDto);
+	
 	
 
 }
