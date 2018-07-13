e3ca7c03b29dd4796c80126ca1ea66f6eda7827a
张贵宏 <zghlx@qq.com>
2018-07-13 15:47:43 +0800

    财务跟单设置后同步权限数据

--- alms-base/src/main/java/com/hongte/alms/base/service/impl/SysFinancialOrderServiceImpl.java
+++ alms-base/src/main/java/com/hongte/alms/base/service/impl/SysFinancialOrderServiceImpl.java
@@ -56,28 +56,28 @@ public class SysFinancialOrderServiceImpl extends BaseServiceImpl<SysFinancialOr
 
 
     /**
-     *  自定义分页查询
+     * 自定义分页查询
      *
-     * @param  businessTypeId, areaId, companyId, userName]
+     * @param businessTypeId, areaId, companyId, userName]
      * @return com.baomidou.mybatisplus.plugins.Page<com.hongte.alms.base.vo.finance.SysFinancialOrderVO>
      * @author 张贵宏
      * @date 2018/6/17 15:25
      */
     @Override
     public Page<SysFinancialOrderVO> search(Page<SysFinancialOrderVO> page, Integer businessTypeId, String areaId, String companyId, String userName) {
-        List<SysFinancialOrderVO>  voList = ((SysFinancialOrderMapper)super.baseMapper).search(page, businessTypeId, areaId, companyId, userName);
+        List<SysFinancialOrderVO> voList = ((SysFinancialOrderMapper) super.baseMapper).search(page, businessTypeId, areaId, companyId, userName);
         return page.setRecords(voList);
     }
 
     /**
-     *  保存数据到父子表
+     * 保存数据到父子表
      *
      * @param
      * @return void
      * @author 张贵宏
      * @date 2018/6/19 9:42
      */
-    @Transactional(rollbackFor = Exception.class)
+    //@Transactional(rollbackFor = Exception.class)
     @Override
     public void save(SysFinancialOrderReq req) {
         for (String companyId : req.getCompanyId()) {
@@ -103,32 +103,47 @@ public class SysFinancialOrderServiceImpl extends BaseServiceImpl<SysFinancialOr
                         throw new RuntimeException("插入失败");
                     }
                 }
+
+                List<SysFinancialOrderUser> financialOrderUsers = sysFinancialOrderUserService.selectList(new EntityWrapper<SysFinancialOrderUser>().eq("finance_order_id", financialOrder.getId()));
+                //先删除再增加
+                sysFinancialOrderUserService.delete(new EntityWrapper<SysFinancialOrderUser>().eq("finance_order_id", financialOrder.getId()));
+                //同步用户权限
+                //同步用户权限
+                if (financialOrderUsers != null && financialOrderUsers.size() > 0) {
+                    for (SysFinancialOrderUser orderUser : financialOrderUsers) {
+                        if (StringUtils.isBlank(orderUser.getUserId())) {
+                            continue;
+                        }
+                        syncUserPermission(orderUser.getUserId());
+                    }
+                }
+
                 for (String userId : req.getCollectionGroup1Users()) {
                     //保证子表中同一父id只有一条记录
-                    SysFinancialOrderUser financialOrderUser = sysFinancialOrderUserService.selectOne(
+                    /* SysFinancialOrderUser financialOrderUser = sysFinancialOrderUserService.selectOne(
                             new EntityWrapper<SysFinancialOrderUser>().eq("finance_order_id", financialOrder.getId())
                                     .eq("user_id", userId)
                     );
-                    if (financialOrderUser == null) {
-                        String userName = sysUserService.selectById(userId).getUserName();
-                        SysFinancialOrderUser newFinancialOrderUser = new SysFinancialOrderUser();
-                        newFinancialOrderUser.setUserId(userId);
-                        newFinancialOrderUser.setUserName(userName);
-                        newFinancialOrderUser.setFinanceOrderId(financialOrder.getId());
-                        newFinancialOrderUser.setCreateUser(loginUserInfoHelper.getUserId());
-                        newFinancialOrderUser.setCreateTime(new Date());
-                        boolean result = sysFinancialOrderUserService.insert(newFinancialOrderUser);
-                        if (!result) {
-                            throw new RuntimeException("插入跟单用户失败");
-                        }
+                    if (financialOrderUser == null) {*/
+                    String userName = sysUserService.selectById(userId).getUserName();
+                    SysFinancialOrderUser newFinancialOrderUser = new SysFinancialOrderUser();
+                    newFinancialOrderUser.setUserId(userId);
+                    newFinancialOrderUser.setUserName(userName);
+                    newFinancialOrderUser.setFinanceOrderId(financialOrder.getId());
+                    newFinancialOrderUser.setCreateUser(loginUserInfoHelper.getUserId());
+                    newFinancialOrderUser.setCreateTime(new Date());
+                    boolean result = sysFinancialOrderUserService.insert(newFinancialOrderUser);
+                    if (!result) {
+                        throw new RuntimeException("插入跟单用户失败");
                     }
+                    //}
                 }
             }
         }
 
         //同步用户权限
         for (String userId : req.getCollectionGroup1Users()) {
-            if(StringUtils.isBlank(userId)){
+            if (StringUtils.isBlank(userId)) {
                 continue;
             }
             syncUserPermission(userId);
@@ -136,14 +151,14 @@ public class SysFinancialOrderServiceImpl extends BaseServiceImpl<SysFinancialOr
     }
 
     /**
-     *  按id删除
+     * 按id删除
      *
      * @param id
      * @return void
      * @author 张贵宏
      * @date 2018/6/19 9:44
      */
-    @Transactional(rollbackFor = Exception.class)
+    //@Transactional(rollbackFor = Exception.class)
     @Override
     public void delete(Integer id) {
         List<SysFinancialOrderUser> financialOrderUsers = sysFinancialOrderUserService.selectList(new EntityWrapper<SysFinancialOrderUser>().eq("finance_order_id", id));
@@ -151,9 +166,9 @@ public class SysFinancialOrderServiceImpl extends BaseServiceImpl<SysFinancialOr
         super.deleteById(id);
 
         //同步用户权限
-        if(financialOrderUsers!=null && financialOrderUsers.size() > 0){
-            for (SysFinancialOrderUser orderUser: financialOrderUsers) {
-                if(StringUtils.isBlank(orderUser.getUserId())){
+        if (financialOrderUsers != null && financialOrderUsers.size() > 0) {
+            for (SysFinancialOrderUser orderUser : financialOrderUsers) {
+                if (StringUtils.isBlank(orderUser.getUserId())) {
                     continue;
                 }
                 syncUserPermission(orderUser.getUserId());
@@ -164,15 +179,16 @@ public class SysFinancialOrderServiceImpl extends BaseServiceImpl<SysFinancialOr
 
     /**
      * 同步用户财务管理用户权限
+     *
      * @param userId 用户id
      */
-    private void syncUserPermission(String userId){
-        executor.execute(()->{
+    private void syncUserPermission(String userId) {
+        executor.execute(() -> {
             LOGGER.info("@财务跟单设置@同步用户财务管理用户权限开始,userId=[{}]", userId);
-            try{
-
-            }catch (Exception ex){
-                LOGGER.error("@财务跟单设置@同步用户财务管理用户权限异常,userId=[{}]",userId, ex);
+            try {
+                sysUserPermissionService.setUserPermissons(userId);
+            } catch (Exception ex) {
+                LOGGER.error("@财务跟单设置@同步用户财务管理用户权限异常,userId=[{}]", userId, ex);
                 Thread.currentThread().interrupt();
             }
             LOGGER.info("@财务跟单设置@同步用户财务管理用户权限结束,userId=[{}]", userId);
