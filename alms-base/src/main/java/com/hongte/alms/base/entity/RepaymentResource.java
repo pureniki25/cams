7a49efc0990ff4505b4b6402705f576f28f41fa0
wangjiguang <wangjiguang@hongte.info>
2018-07-16 20:46:18 +0800

    屏蔽settle_log_id

--- alms-base/src/main/java/com/hongte/alms/base/entity/RepaymentResource.java
+++ alms-base/src/main/java/com/hongte/alms/base/entity/RepaymentResource.java
@@ -127,7 +127,7 @@ public class RepaymentResource extends Model<RepaymentResource> {
 	/**
 	 * 结清还款确认Id
 	 */
-	@TableField("settle_log_id")
+//	@TableField("settle_log_id")
 	@ApiModelProperty(required= true,value = "结清还款确认Id")
 	private String settleLogId;
 
