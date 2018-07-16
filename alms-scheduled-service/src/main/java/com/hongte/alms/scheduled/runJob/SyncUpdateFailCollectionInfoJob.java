323860a4a80b96941b6b946d2affb9a28c908aa4
zengkun <zengkun5200@163.com>
2018-07-16 22:01:57 +0800

    电催同步失败的数据重新同步定时调度任务

--- alms-scheduled-service/src/main/java/com/hongte/alms/scheduled/runJob/SyncUpdateFailCollectionInfoJob.java
+++ alms-scheduled-service/src/main/java/com/hongte/alms/scheduled/runJob/SyncUpdateFailCollectionInfoJob.java
@@ -14,6 +14,7 @@ import com.xxl.job.core.handler.annotation.JobHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -32,6 +33,7 @@ public class SyncUpdateFailCollectionInfoJob extends IJobHandler {
 
 
     @Autowired
+    @Qualifier("IssueSendOutsideLogService")
     IssueSendOutsideLogService issueSendOutsideLogService;
 
 
