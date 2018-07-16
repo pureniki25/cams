525feb99c0cd8ea3e438f36cc27fb95334b5460a
张贵宏 <zghlx@qq.com>
2018-07-16 09:48:31 +0800

    代扣平台代扣流水基础类生成

---
+++ alms-base/src/main/java/com/hongte/alms/base/mapper/WithholdingFlowRecordMapper.java
@@ -0,0 +1,16 @@
+package com.hongte.alms.base.mapper;
+
+import com.hongte.alms.base.entity.WithholdingFlowRecord;
+import com.hongte.alms.common.mapper.SuperMapper;
+
+/**
+ * <p>
+ * 代扣平台代扣流水 Mapper 接口
+ * </p>
+ *
+ * @author 张贵宏
+ * @since 2018-07-16
+ */
+public interface WithholdingFlowRecordMapper extends SuperMapper<WithholdingFlowRecord> {
+
+}
---
+++ alms-base/src/main/java/com/hongte/alms/base/service/WithholdingFlowRecordService.java
@@ -0,0 +1,16 @@
+package com.hongte.alms.base.service;
+
+import com.hongte.alms.base.entity.WithholdingFlowRecord;
+import com.hongte.alms.common.service.BaseService;
+
+/**
+ * <p>
+ * 代扣平台代扣流水 服务类
+ * </p>
+ *
+ * @author 张贵宏
+ * @since 2018-07-16
+ */
+public interface WithholdingFlowRecordService extends BaseService<WithholdingFlowRecord> {
+
+}
