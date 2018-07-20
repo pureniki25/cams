fefa39d6fca621f7dd31b3143fd44991e4e38c3d
liuzhengquan@hongte.info <liu@2018>
2018-07-20 17:43:15 +0800

    mq配置

--- alms-core-service/src/main/java/com/hongte/alms/core/config/AmqpConfig.java
+++ alms-core-service/src/main/java/com/hongte/alms/core/config/AmqpConfig.java
@@ -7,11 +7,15 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+
+import com.rabbitmq.client.ConnectionFactory;
 
 @Configuration
 public class AmqpConfig {
@@ -85,6 +89,14 @@ public class AmqpConfig {
         rabbitMessagingTemplate.setMessageConverter(jackson2Converter());
         rabbitMessagingTemplate.setRabbitTemplate(rabbitTemplate);
         return rabbitMessagingTemplate;
+    }
+    
+    
+    @Bean
+    public ConnectionFactory connectionFactory() {
+    	ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("/uc");
+        return factory;
     }
 
 }
