97dac8786638a712cb2a83028ffad4b0a5091a59
chenzesheng <chenzesheng@hongte.info>
2018-07-23 10:22:54 +0800

    滞纳金计算重新开启一个事务

--- alms-base/src/main/java/com/hongte/alms/base/service/impl/RepaymentProjPlanListServiceImpl.java
+++ alms-base/src/main/java/com/hongte/alms/base/service/impl/RepaymentProjPlanListServiceImpl.java
@@ -46,6 +46,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * <p>
@@ -597,7 +599,7 @@ public class RepaymentProjPlanListServiceImpl extends
 		}
 
 
-
+	@Transactional(propagation = Propagation.REQUIRES_NEW, noRollbackFor = Exception.class)
 	@Override
 	public RepaymentBizPlanList calLateFeeForPerPList(RepaymentBizPlanList pList) {
 		Date nowDate=new Date();
