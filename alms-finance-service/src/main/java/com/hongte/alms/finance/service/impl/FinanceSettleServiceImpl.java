47b56947e9086067b1e97e937efbf263422449fb
wangjiguang <wangjiguang@hongte.info>
2018-08-16 15:59:46 +0800

    修改找当前期的sql 修改还款计划列表的时区问题

--- alms-finance-service/src/main/java/com/hongte/alms/finance/service/impl/FinanceSettleServiceImpl.java
+++ alms-finance-service/src/main/java/com/hongte/alms/finance/service/impl/FinanceSettleServiceImpl.java
@@ -2080,7 +2080,8 @@ public class FinanceSettleServiceImpl implements FinanceSettleService {
                     new EntityWrapper<RepaymentBizPlanList>()
                             .eq("business_id", now.getBusinessId())
                             .eq("plan_id", repaymentBizPlan.getPlanId())
-                            .gt("due_date", new Date())
+//                            .ge("due_date", new Date())
+                            .and(" DATE(due_date) >= DATE({0}) ", new Date())
                             .eq("current_status", RepayCurrentStatusEnums.还款中.toString())
                             .orderBy("due_date"));
             if (CollectionUtils.isEmpty(selectList)) {
@@ -2130,8 +2131,8 @@ public class FinanceSettleServiceImpl implements FinanceSettleService {
 			// 找应还日期离当前日期最近且为还款中的期数作为当前期
 			List<RepaymentBizPlanList> selectList = repaymentBizPlanListMapper
 					.selectList(new EntityWrapper<RepaymentBizPlanList>().eq("business_id", req.getBusinessId())
-							.eq("plan_id", repaymentBizPlan.getPlanId()).gt("due_date", settleDate)
-							.eq("current_status", RepayCurrentStatusEnums.还款中.toString()).orderBy("due_date"));
+							.eq("plan_id", repaymentBizPlan.getPlanId()).and(" DATE(due_date) >= DATE({0}) ", settleDate)
+							.eq("current_status", RepayCurrentStatusEnums.还款中.toString()).orderBy("due_date",false));
 			// 判断当前期列表是否为空
 			if (CollectionUtils.isEmpty(selectList)) {
 				// 找不到还款中的当前期则判断结清日期是否大过还款计划最后一次还款的期限
@@ -2149,9 +2150,9 @@ public class FinanceSettleServiceImpl implements FinanceSettleService {
 			// 再次判断当前期列表是否为空
 			if (CollectionUtils.isEmpty(selectList)) {
 				if (!StringUtil.isEmpty(req.getPlanId())) {
-					logger.error("找此业务还款计划的当前期 RepaymentBizPlanList  businessId:" + req.getBusinessId() + "     planId:"
+					logger.error("找不到此业务还款计划的当前期 RepaymentBizPlanList  businessId:" + req.getBusinessId() + "     planId:"
 							+ repaymentBizPlan.getPlanId());
-					throw new SettleRepaymentExcepiton("找此业务还款计划的当前期",
+					throw new SettleRepaymentExcepiton("找不到此业务还款计划的当前期",
 							ExceptionCodeEnum.NO_BIZ_PLAN_LIST.getValue().toString());
 				} else {
 					continue;
@@ -2212,7 +2213,8 @@ public class FinanceSettleServiceImpl implements FinanceSettleService {
 					new EntityWrapper<RepaymentBizPlanList>()
 					.eq("business_id", req.getBusinessId())
 					.eq("plan_id", repaymentBizPlan.getPlanId())
-					.gt("due_date", settleDate)
+//					.ge("due_date", settleDate)
+					.and(" DATE(due_date) >= DATE({0}) ",settleDate)
 					.eq("current_status", RepayCurrentStatusEnums.还款中.toString())
 					.orderBy("due_date"));
     		//判断当前期列表是否为空
