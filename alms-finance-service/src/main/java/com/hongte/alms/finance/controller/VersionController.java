b18b18c20d2bb0ef6f24166e5b1f5f32b5baa669
wangjiguang <wangjiguang@hongte.info>
2018-08-27 10:49:43 +0800

    各个服务添加版本查询,用于排除线上版本问题

---
+++ alms-finance-service/src/main/java/com/hongte/alms/finance/controller/VersionController.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.hongte.alms.finance.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hongte.alms.base.controller.VersionAdapter;
+
+/**
+ * @author 王继光
+ * 2018年8月27日 上午10:09:37
+ */
+@RestController
+public class VersionController extends VersionAdapter{
+
+}
