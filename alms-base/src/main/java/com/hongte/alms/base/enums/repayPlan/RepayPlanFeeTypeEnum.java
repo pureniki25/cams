1735b0263938dab922e0bba4118d65c1eb9d429c
wangjiguang <wangjiguang@hongte.info>
2018-08-31 09:14:19 +0800

    同步信贷系统 提前违约金

--- alms-base/src/main/java/com/hongte/alms/base/enums/repayPlan/RepayPlanFeeTypeEnum.java
+++ alms-base/src/main/java/com/hongte/alms/base/enums/repayPlan/RepayPlanFeeTypeEnum.java
@@ -23,10 +23,10 @@ public enum RepayPlanFeeTypeEnum implements IEnum {
     OVER_DUE_AMONT_ONLINE(60,"线上滞纳金","2e646c87-5721-11e8-8a00-0242ac110002",5),
     OVER_DUE_AMONT_UNDERLINE(60,"线下滞纳金","3131c075-5721-11e8-8a00-0242ac110002",5),
     LACK_FEE(61,"往期少缴费用","aks29304-5721-11e8-8a00-0242ac110002",61),
-    PENALTY_AMONT(70,"违约金","79069922-e13a-4229-8656-2a1e19b44879",5)
-    ,PRINCIPAL_PENALTY(70,"本金违约金","9883ee39-4448-11e8-b4f8-0242ac110003",5)  //uuid与信贷 固定费用id 一致
-    ,SUB_COMPANY_PENALTY(70,"分公司月收服务费违约金","9e0a6fc1-4448-11e8-b4f8-0242ac110003",5)  //uuid与信贷 固定费用id 一致
-    ,PLAT_PENALTY(70,"平台服务费违约金","a0b666c2-4448-11e8-b4f8-0242ac110003",5)   // uuid与信贷 固定费用id 一致
+    PENALTY_AMONT(70,"违约金","79069922-e13a-4229-8656-2a1e19b44879",4)
+    ,PRINCIPAL_PENALTY(70,"本金违约金","9883ee39-4448-11e8-b4f8-0242ac110003",4)  //uuid与信贷 固定费用id 一致
+    ,SUB_COMPANY_PENALTY(70,"分公司月收服务费违约金","9e0a6fc1-4448-11e8-b4f8-0242ac110003",4)  //uuid与信贷 固定费用id 一致
+    ,PLAT_PENALTY(70,"平台服务费违约金","a0b666c2-4448-11e8-b4f8-0242ac110003",4)   // uuid与信贷 固定费用id 一致
     ,AGENCY_FEE(80,"中介费","fe27638a-91a8-4555-920e-9c6a535e7781",4),
     DEPOSIT_FEE(90,"押金类费用","3aeef26a-9685-4148-98cb-fa3f88181c67",4)
     ,RUSH_RECEIVABLES(100,"冲应收","d5596127-530f-4083-95c5-01e5a75635aa",6)
