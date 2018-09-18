32042f2fb51e2b947557eb2fe35ded8fef795172
liuzhengquan@hongte.info <liu@2018>
2018-09-18 15:30:37 +0800

    结余转还款 问题

--- alms-finance-service/src/main/java/com/hongte/alms/finance/controller/CamsFlowController.java
+++ alms-finance-service/src/main/java/com/hongte/alms/finance/controller/CamsFlowController.java
@@ -588,7 +588,7 @@ public class CamsFlowController {
             		flow.setSourceAccountIdentifierId(sourceAccountIdentifierId);
                 	flow.setTargetAccountIdentifierId(null);
             	}
-            	if(repayType != 7 || flowMap.size() == 1) {
+            	if(repayType != 7 || listOnlineFlow.size() == 1) {
             		flows.add(flow);
             	}
             	Map<String,Object> paramFlowItemMap = new HashMap<>();
