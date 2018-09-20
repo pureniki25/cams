2d5972895b872b297519af73619631e83983f9f4
huweiqian <huweiqian@hongte.info>
2018-09-20 16:15:15 +0800

    加版本号

--- alms-base/src/main/java/com/hongte/alms/base/controller/VersionAdapter.java
+++ alms-base/src/main/java/com/hongte/alms/base/controller/VersionAdapter.java
@@ -18,7 +18,7 @@ public class VersionAdapter {
 	/*Cservice="2018-08-27 08:45:00"*/
 	/*Dservice="1"*/
 	final String ALMS_CORE_SERVICE = "(finance)0920 16:05" ;
-	final String ALMS_FINANCE_SERVICE = "ALMS0919" ;
+	final String ALMS_FINANCE_SERVICE = "(finance)0920 16:14" ;
 	final String ALMS_OPEN_SERVICE = "0913" ;
 	final String ALMS_PLATREPAY_SERVICE = "0918 10:26:27" ;
 	final String ALMS_SCHEDULED_SERVICE = "(finance)0920 16:05" ;
