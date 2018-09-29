962d0acb698908cfc0fd2ebd2407ae638b0968f2
liuzhengquan@hongte.info <liu@2018>
2018-09-29 18:04:53 +0800

    流水推送

--- alms-base/src/main/java/com/hongte/alms/base/controller/VersionAdapter.java
+++ alms-base/src/main/java/com/hongte/alms/base/controller/VersionAdapter.java
@@ -18,10 +18,10 @@ public class VersionAdapter {
 	/*Cservice="2018-08-27 08:45:00"*/
 	/*Dservice="1"*/
 	final String ALMS_CORE_SERVICE = "(finance)0929 14:06" ;
-	final String ALMS_FINANCE_SERVICE = "(finance)0928 20:22" ;
+	final String ALMS_FINANCE_SERVICE = "(finance)0929 20:22" ;
 	final String ALMS_OPEN_SERVICE = "0913" ;
 	final String ALMS_PLATREPAY_SERVICE = "092710:26:27" ;
-	final String ALMS_SCHEDULED_SERVICE = "(ALMS-finance)0927 16:50" ;
+	final String ALMS_SCHEDULED_SERVICE = "(ALMS-finance)0929 16:50" ;
 
 	final String ALMS_WEBUI = "(finance)2018-09-13 11:33" ; 
 	final String ALMS_WITHHOLD_SERVICE = "(finance)0926 22:16" ;
