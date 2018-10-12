498c1ec0aa36876c066e7bade7f710004542931e
wangjiguang <wangjiguang@hongte.info>
2018-10-12 18:42:57 +0800

    开发实还记录功能 导出Excel

--- alms-finance-service/src/main/java/com/hongte/alms/finance/controller/RepaymentConfirmLogSynchController.java
+++ alms-finance-service/src/main/java/com/hongte/alms/finance/controller/RepaymentConfirmLogSynchController.java
@@ -1,9 +1,36 @@
 package com.hongte.alms.finance.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.plugins.Page;
+import com.hongte.alms.base.dto.FactRepayReq;
+import com.hongte.alms.base.entity.RepaymentConfirmLogSynch;
+import com.hongte.alms.base.service.RepaymentConfirmLogSynchService;
+
+import cn.afterturn.easypoi.entity.vo.MapExcelConstants;
+import cn.afterturn.easypoi.entity.vo.NormalExcelConstants;
+import cn.afterturn.easypoi.excel.entity.ExportParams;
+import cn.afterturn.easypoi.excel.entity.enmus.ExcelType;
+import cn.afterturn.easypoi.view.PoiBaseView;
 
 /**
  * <p>
@@ -13,9 +40,42 @@ import org.springframework.web.bind.annotation.RestController;
  * @author wangjiguang
  * @since 2018-10-09
  */
-@RestController
-@RequestMapping("/repaymentConfirmLogSynch")
+@Controller
+@RequestMapping("/factRepay")
 public class RepaymentConfirmLogSynchController {
 
+	private static Logger LOGGER = LoggerFactory.getLogger(RepaymentConfirmLogSynchController.class);
+	
+	@Autowired
+	@Qualifier("RepaymentConfirmLogSynchService")
+	private RepaymentConfirmLogSynchService synchService ;
+	
+	@PostMapping("/list")
+	@ResponseBody
+	public Page<RepaymentConfirmLogSynch> list (@RequestBody FactRepayReq req){
+		if (LOGGER.isInfoEnabled()) {
+			LOGGER.info("@factRepay@list--开始{}",JSON.toJSONString(req));
+		}
+		return synchService.page(req) ;
+	}
+	
+	@RequestMapping("/export")
+    public void download(ModelMap modelMap , HttpServletRequest request,HttpServletResponse response) {
+		FactRepayReq req = new FactRepayReq() ;
+		req.setCurPage(0);
+        List<RepaymentConfirmLogSynch> repaymentConfirmLogSynchs = synchService.select(req);
+
+        
+        ExportParams params = new ExportParams("导出文件", "测试", ExcelType.XSSF);
+        
+        modelMap.put(NormalExcelConstants.DATA_LIST, repaymentConfirmLogSynchs); 
+        modelMap.put(NormalExcelConstants.CLASS, RepaymentConfirmLogSynch.class);
+        modelMap.put(NormalExcelConstants.PARAMS, params);//参数
+        modelMap.put(NormalExcelConstants.FILE_NAME, params.getTitle());//文件名称
+        
+        PoiBaseView.render(modelMap, request, response, NormalExcelConstants.EASYPOI_EXCEL_VIEW);
+//        return MapExcelConstants.EASYPOI_MAP_EXCEL_VIEW;//View名称
+
+    }
 }
 
