f5b150d82d920a0bef52d16361754a9a6956f03d
wangjiguang <wangjiguang@hongte.info>
2018-10-19 14:42:52 +0800

    开发实还记录功能

--- alms-base/src/main/java/com/hongte/alms/base/controller/VersionAdapter.java
+++ alms-base/src/main/java/com/hongte/alms/base/controller/VersionAdapter.java
@@ -18,7 +18,7 @@ public class VersionAdapter {
 	/*Cservice="2018-08-27 08:45:00"*/
 	/*Dservice="1"*/
 	final String ALMS_CORE_SERVICE = "(finance)1018 18:10" ;
-	final String ALMS_FINANCE_SERVICE = "(finance)1018 18:10" ;
+	final String ALMS_FINANCE_SERVICE = "(finance)1019 14:42 实还记录操作人员" ;
 	final String ALMS_OPEN_SERVICE = "0913" ;
 	final String ALMS_PLATREPAY_SERVICE = "(finance)1018 18:10" ;
 	final String ALMS_SCHEDULED_SERVICE = "(finance)1018 18:10" ;
