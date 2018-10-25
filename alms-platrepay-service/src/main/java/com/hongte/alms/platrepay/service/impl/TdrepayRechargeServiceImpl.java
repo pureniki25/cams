b69fd80d171b51d19554eb5059ede8705724f08e
huweiqian <huweiqian@hongte.info>
2018-10-25 15:28:14 +0800

    还原版本

--- alms-platrepay-service/src/main/java/com/hongte/alms/platrepay/service/impl/TdrepayRechargeServiceImpl.java
+++ alms-platrepay-service/src/main/java/com/hongte/alms/platrepay/service/impl/TdrepayRechargeServiceImpl.java
@@ -249,7 +249,7 @@ public class TdrepayRechargeServiceImpl implements TdrepayRechargeService {
 			/*
 			 * 将分发状态更新为处理中 分发状态（0：待分发，1：分发处理中，2：分发成功，3，分发失败）
 			 */
-			updateTdrepayRechargeLogProcessStatus(vos, 1, userId, "分发处理中");
+			updateTdrepayRechargeLogProcessStatus(vos, 1, userId);
 
 			/*
 			 * 由于每种业务类型对应一个资产端账户唯一编号，根据业务类型进行分批
@@ -273,7 +273,7 @@ public class TdrepayRechargeServiceImpl implements TdrepayRechargeService {
 			/*
 			 * 将分发状态更新为处理中 分发状态（0：待分发，1：分发处理中，2：分发成功，3，分发失败）
 			 */
-			updateTdrepayRechargeLogProcessStatus(vos, 3, userId, "系统异常，分发失败");
+			updateTdrepayRechargeLogProcessStatus(vos, 3, userId);
 
 			throw new ServiceRuntimeException("资金分发执行失败", e);
 		}
@@ -317,7 +317,7 @@ public class TdrepayRechargeServiceImpl implements TdrepayRechargeService {
 	@SuppressWarnings("rawtypes")
 	private void handleSendDistributeFundResult(List<TdrepayRechargeInfoVO> vos, String userId, Result result) {
 		if (result == null || !Constant.REMOTE_EIP_SUCCESS_CODE.equals(result.getReturnCode())) {
-			updateTdrepayRechargeLogProcessStatus(vos, 3, userId, result.getCodeDesc());
+			updateTdrepayRechargeLogProcessStatus(vos, 3, userId);
 		}
 	}
 
@@ -329,7 +329,7 @@ public class TdrepayRechargeServiceImpl implements TdrepayRechargeService {
 	 * @param userId
 	 */
 	private List<TdrepayRechargeLog> updateTdrepayRechargeLogProcessStatus(List<TdrepayRechargeInfoVO> vos,
-			Integer processStatus, String userId, String remark) {
+			Integer processStatus, String userId) {
 		List<TdrepayRechargeLog> rechargeLogs = new LinkedList<>();
 		for (TdrepayRechargeInfoVO vo : vos) {
 			TdrepayRechargeLog tdrepayRechargeLog = new TdrepayRechargeLog();
