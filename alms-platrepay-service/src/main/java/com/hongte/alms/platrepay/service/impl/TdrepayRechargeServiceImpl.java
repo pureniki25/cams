c22f18b3d3d18716e17d4a7c8ac5708bcc082ccf
huweiqian <huweiqian@hongte.info>
2018-10-27 16:06:55 +0800

    修复bug

--- alms-platrepay-service/src/main/java/com/hongte/alms/platrepay/service/impl/TdrepayRechargeServiceImpl.java
+++ alms-platrepay-service/src/main/java/com/hongte/alms/platrepay/service/impl/TdrepayRechargeServiceImpl.java
@@ -1480,10 +1480,10 @@ public class TdrepayRechargeServiceImpl implements TdrepayRechargeService {
 
 		if (isSettle) {
 			rechargeLogs = tdrepayRechargeLogService.selectList(new EntityWrapper<TdrepayRechargeLog>()
-					.eq("is_valid", 1).in("status", lstStatus).eq("process_status", 2).ne("settle_type", 0).where("order by project_id, period"));
+					.eq("is_valid", 1).in("status", lstStatus).eq("process_status", 2).ne("settle_type", 0).orderBy("project_id, period"));
 		} else {
 			rechargeLogs = tdrepayRechargeLogService.selectList(new EntityWrapper<TdrepayRechargeLog>()
-					.eq("is_valid", 1).in("status", lstStatus).eq("process_status", 2).where("order by project_id, period"));
+					.eq("is_valid", 1).in("status", lstStatus).eq("process_status", 2).orderBy("project_id, period"));
 		}
 
 		if (CollectionUtils.isEmpty(rechargeLogs)) {
