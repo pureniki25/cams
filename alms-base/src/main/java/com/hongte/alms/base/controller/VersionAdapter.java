88bab9f7bd96342184fa98287c9eab936ec7d35f
huweiqian <huweiqian@hongte.info>
2018-11-08 15:41:55 +0800

    增加版本号

--- alms-base/src/main/java/com/hongte/alms/base/controller/VersionAdapter.java
+++ alms-base/src/main/java/com/hongte/alms/base/controller/VersionAdapter.java
@@ -21,7 +21,7 @@ public class VersionAdapter {
 	final String ALMS_FINANCE_SERVICE = "(prdbug)1027 11:30" ;
 	final String ALMS_OPEN_SERVICE = "(prdbug)1026 20:35" ;
 	final String ALMS_PLATREPAY_SERVICE = "(platrepay)1029 17:10" ;
-	final String ALMS_SCHEDULED_SERVICE = "(platrepay)1029 17:10" ;
+	final String ALMS_SCHEDULED_SERVICE = "(prdbug)1108 15:10" ;
 
 	final String ALMS_WEBUI = "(prdbug)1030 19:49 线下补录的单在贷后管理不显示详情" ;
 	final String ALMS_WITHHOLD_SERVICE = "(prdbug)1108 15:10" ;
